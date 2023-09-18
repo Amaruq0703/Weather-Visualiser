475d8168bf55265e8866fa9fcec63af89f2fbc6d
Aditya Khera <aditya.khera7@gmail.com>
2023-09-18 17:40:28 +0100

    backend change

--- backend.py
+++ backend.py
@@ -1,8 +1,8 @@
 import requests
-from config import APIkey
+
 
 def get_data(place, forecastdays): 
-    
+    APIkey = 'b0262bf4ebad231dd829d5ff62348f0e'
     url = f'https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={APIkey}'   
     response = requests.get(url)
     data = response.json()
